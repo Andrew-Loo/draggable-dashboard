d18522dc3abfa50d7c8a831e45407c66d178a43f
Andrew Loo <57743033+Andrew-Loo@users.noreply.github.com>
2025-10-14 18:06:37 +0800

    Initial commit

---
+++ src/components/ChartWidget.jsx
@@ -0,0 +1,11 @@
+export default function ChartWidget() {
+  return (
+    <div>
+      <div className="mb-1">📈 This is a placeholder chart.</div>
+      <ul className="list-disc pl-5 space-y-1">
+        <li>Drag corners to resize</li>
+        <li>Drag header (in edit mode) to move</li>
+      </ul>
+    </div>
+  );
+}
