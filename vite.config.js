d2ae6295472f23536e91c3b9611f31dab4defeb6
Andrew Loo <57743033+Andrew-Loo@users.noreply.github.com>
2025-10-15 16:05:19 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // IMPORTANT: keep this base in sync with your repo name
 export default defineConfig({
-  base: "/draggable-dashboard/",
+  base: "/",
   plugins: [react()]
 });
